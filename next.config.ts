7258e376d31c031fd94645e965829bbbe4b9abba
saim khan <salim.khan@mobulous.com>
2025-01-06 18:55:10 +0530

    correct config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enables React Strict Mode
+  swcMinify: true, // Enables SWC-based minification
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Example of customizing the Webpack configuration for client-side
+      config.resolve.fallback = { fs: false };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
